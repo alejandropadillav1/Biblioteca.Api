e9fcde24b991f27c66a91f16e68244a58553bdf6
Alejandro Padilla <apadilla@arrivelogistics.com>
2022-05-06 13:13:22 -0500

    Add project files.

---
+++ Biblioteca.Api/Controllers/BibliotecaController.cs
@@ -0,0 +1,124 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Api.Data;
+using Biblioteca.Api.Modelo;
+
+namespace Biblioteca.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class BibliotecaController : ControllerBase
+    {
+        private readonly BibliotecaApiContext _context;
+
+        public BibliotecaController(BibliotecaApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok(await _context.Libro.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.Libro
+                .FirstOrDefaultAsync(m => m.IdLibro == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(libro);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("IdLibro,Titulo,Editorial,Autor,Pagina,Descripcion")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(libro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return Ok(libro);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Titulo,Editorial,Autor,Pagina,Descripcion")] Libro libro)
+        {
+            if (id != libro.IdLibro)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(libro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LibroExists(libro.IdLibro))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return Ok(libro);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.Libro
+                .FirstOrDefaultAsync(m => m.IdLibro == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(libro);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var libro = await _context.Libro.FindAsync(id);
+            _context.Libro.Remove(libro);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LibroExists(int id)
+        {
+            return _context.Libro.Any(e => e.IdLibro == id);
+        }
+    }
+}
